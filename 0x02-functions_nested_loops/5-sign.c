2edf1125d8d6a173fa9424b583dc9929fb300b2c
ObakengPitse <obakengpitse@yahoo.com>
2023-03-21 14:47:53 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign of a number
+ *
+ * @n: input number
+ *
+ * Return: 1 or 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
