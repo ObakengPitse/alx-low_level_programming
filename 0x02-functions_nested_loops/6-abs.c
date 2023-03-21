b8920758464f24a40bc54f128e27b3698c0b8d01
ObakengPitse <obakengpitse@yahoo.com>
2023-03-21 14:53:48 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - return absulute of a number
+ *
+ * @c: input number
+ *
+ * Return: absulute number
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
