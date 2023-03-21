e2d5b4a91574fd5111b0004cd7bc4f0c43872b83
ObakengPitse <obakengpitse@yahoo.com>
2023-03-21 15:16:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: num1
+ * @b: num2
+ * Return: Results of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * jack_bauer - clock 
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int x;
+	int y
+	for 
