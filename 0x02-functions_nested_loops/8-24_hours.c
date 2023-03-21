24e8b16e7def1cc6107fed882a0300b98b2e9a4e
ObakengPitse <obakengpitse@yahoo.com>
2023-03-21 15:27:45 -0700

    8-244_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * jack_bauer - clock 
+ * jack_bauer - clock
  *
  * Return: Always 0
  */
 void jack_bauer(void)
 {
 	int x;
-	int y
-	for 
+	int y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
